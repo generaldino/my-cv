0a34772cf2a63c9e008dfcb076b03ecd4ebd1965
Danny Hakim <60073397+generaldino@users.noreply.github.com>
2024-02-10 13:06:00 +0000

    added more details

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -4,6 +4,8 @@ import Ucl from "../../public/ucl.svg";
 import Imperial from "../../public/imperial.svg";
 import Jpm from "../../public/jpm.svg";
 import Qnt from "../../public/qnt.svg";
+import Lit from "../../public/lit.svg";
+import Compose from "../../public/compose.svg";
 
 export const RESUME_DATA = {
   name: "Danny Hakim",
@@ -13,7 +15,7 @@ export const RESUME_DATA = {
   about:
     "Full stack product leader, with track record of figuring out what to do & making it happen",
   summary:
-    "Product Leader happy to roll up his sleeves and join in designing or writing code to get things done.Passionate about building and working with high-performing, cross-functional teams, to transform innovative ideas into market-ready products to test informed hypotheses and get a product that resonates with users. At my best wearing multiple hats, whether it's strategy, designing, coding, or collaborating with colleagues.",
+    "Product Leader happy to roll up his sleeves and join in designing or writing code to get things done. Passionate about building and working with high-performing, cross-functional teams, to transform innovative ideas into market-ready products to test informed hypotheses and get a product that resonates with users. At my best wearing multiple hats, whether it's strategy, designing, coding, or collaborating with colleagues.",
   avatarUrl:
     "https://avatars.githubusercontent.com/u/60073397?s=400&u=d5db1cbd76876a2ead2cb75b2478a8d5698d8b7b&v=4",
   // personalWebsiteUrl: "https://jarocki.me",
@@ -56,6 +58,22 @@ export const RESUME_DATA = {
   ],
   work: [
     {
+      company: "Compose",
+      link: "https://compose.im/",
+      badges: ["London, UK"],
+      title: "Co-founder, Product & Engineering",
+      logo: Compose,
+      start: "Dec 2020",
+      end: "Present",
+      description: [
+        "Ensured team understood customers problems and empowered to deliver innovative solutions",
+        "Managed day to day of the Product, Design and Engineering team (12+ individuals) ",
+        "Worked with Design & Engineering team to flesh out solutions and review work and jam with the team to overcome any blockers encountered",
+        "Frequently joined in designing or writing code to get things done with my team",
+        "Fostered a culture of trust & accountability across the team. Instilled a “Yes, and…” mindset to set ego aside and productively achieve winning solutions",
+      ],
+    },
+    {
       company: "Quanterium",
       link: "https://www.linkedin.com/company/quanterium-tech/",
       badges: ["London, UK"],
@@ -64,10 +82,23 @@ export const RESUME_DATA = {
       start: "Oct 2018",
       end: "Dec 2020",
       description: [
-        "Identify key problems faced by customers and define strategy to solve them through our product",
+        "Identified key problems faced by customers and define strategy to solve them through our product",
         "Led sales effort marketing the product to prospect clients with 15 financial institutions onboarded",
         "Partnered with Technical Co-founder to build trading platform",
         "Worked with external lawyers to define & build Compliance & KYC process to onboard customers",
+      ],
+    },
+    {
+      company: "Levant in Tech",
+      link: "https://www.linkedin.com/company/levant-in-tech/",
+      badges: ["London, UK"],
+      title: "Co-founder",
+      logo: Lit,
+      start: "Nov 2017",
+      end: "Present",
+      description: [
+        "Organise & host quarterly in person networking events for our community members",
+        "Talented community members from every part of tech venture capital, big tech & startups",
       ],
     },
     {
@@ -112,7 +143,7 @@ export const RESUME_DATA = {
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Co-founder",
       techStack: [
         "Side Project",
         "TypeScript",
@@ -122,7 +153,7 @@ export const RESUME_DATA = {
         "WebRTC",
       ],
       description: "A platform to build and grow your online business",
-      // logo: ConsultlyLogo,
+      logo: Lit,
       link: {
         label: "consultly.com",
         href: "https://consultly.com/",
