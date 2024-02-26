6e5e772c55941fe6377fb8d7b7c6ef7cee63951b
Danny Hakim <60073397+generaldino@users.noreply.github.com>
2024-02-26 11:36:37 +0000

    moved to svg files & seperated types in to seperate file

---
+++ src/data/resume-data.types.ts
@@ -0,0 +1,72 @@
+import { IconButtonProps } from "@/components/ui/icon-button";
+
+export interface SocialLink {
+  name: string;
+  url: string;
+  icon: IconButtonProps;
+}
+
+export interface Tool {
+  name: string;
+  categoryId: string;
+  logo: string;
+  description: string;
+  link: string;
+}
+
+export interface Category {
+  name: string;
+  icon: string;
+}
+
+export interface Education {
+  school: string;
+  degree: string;
+  logo: string;
+  start: string;
+  end: string;
+}
+
+export interface WorkExperience {
+  company: string;
+  link: string;
+  badges: string[];
+  title: string;
+  logo: string;
+  start: string;
+  end: string;
+  about: string;
+  description: string[];
+}
+
+export interface Reference {
+  name: string;
+  link: string;
+  role: string;
+  description: string;
+}
+
+export interface ResumeData {
+  name: string;
+  initials: string;
+  location: string;
+  locationLink: string;
+  about: string;
+  summary: string;
+  avatarUrl: string;
+  personalWebsiteUrl?: string;
+  contact: {
+    email: string;
+    tel: string;
+    social: SocialLink[];
+  };
+  values: string[];
+  tool_categories: {
+    [key: string]: Category;
+  };
+  tools: Tool[];
+  education: Education[];
+  work: WorkExperience[];
+  skills: string[];
+  references: Reference[];
+}
