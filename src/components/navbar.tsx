369a327be618183d9050a9a8d2899e4ba58059e2
Danny Hakim <60073397+generaldino@users.noreply.github.com>
2024-03-05 16:14:59 +0000

    fixed styling for dark theme

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,18 +7,20 @@ const Navbar = () => {
   return (
     <nav className="fixed left-0 right-0 top-0 z-10 bg-background">
       <div className="container mx-auto flex items-center justify-between px-6 py-3">
-        <div>
-          <Link href="/">
-            <Image src={Logo} alt="" width={40} height={40} />
-          </Link>
-        </div>
-        <div>
-          <Link href="/#about">About</Link>
-          <Link href="/#work_experience">Work Experience</Link>
-          <Link href="/#values">Values</Link>
-          <Link href="/#references">References</Link>
-          <Link href="/#education">Education</Link>
-          <Link href="/#tooling">Tooling</Link>
+        <div className="flex-start flex items-center gap-5">
+          <div>
+            <Link href="/">
+              <Image src={Logo} alt="" width={40} height={40} />
+            </Link>
+          </div>
+          <div>
+            <Link href="/#about">About</Link>
+            <Link href="/#work_experience">Work Experience</Link>
+            <Link href="/#values">Values</Link>
+            <Link href="/#references">References</Link>
+            <Link href="/#education">Education</Link>
+            <Link href="/#tooling">Tooling</Link>
+          </div>
         </div>
         <ModeToggle />
       </div>
