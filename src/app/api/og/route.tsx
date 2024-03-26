151394f40c05fa9b92ed7ba9736f8c13a93c0dd4
Danny Hakim <60073397+generaldino@users.noreply.github.com>
2024-03-26 13:33:42 +0000

    og-image

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -6,8 +6,8 @@ export async function GET() {
   return new ImageResponse(
     (
       <img
-        src="https://baileymatthews.dev/bailey-matthews.jpeg"
-        alt="Bailey Matthews"
+        src="https://github.com/generaldino/my-cv/blob/main/public/og-image.jpg?raw=true"
+        alt="og-image"
         width={1200}
         height={600}
       />
