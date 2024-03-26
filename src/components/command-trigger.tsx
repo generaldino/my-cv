52b2da9b4c6f40cb56cd0cbd63492389ce6fe102
Danny Hakim <60073397+generaldino@users.noreply.github.com>
2024-03-26 16:05:55 +0000

    tweak

--- src/components/command-trigger.tsx
+++ src/components/command-trigger.tsx
@@ -30,10 +30,10 @@ export const CommandTrigger = ({ links }: Props) => {
       <div className="w-full flex-1 md:w-auto md:flex-none">
         <button
           onClick={() => setOpen((open) => !open)}
-          className="flex h-full w-full items-center justify-start gap-20 whitespace-nowrap rounded-[0.5rem] border border-input bg-background px-4 py-2 text-sm font-normal text-muted-foreground shadow-none transition-colors hover:bg-accent hover:text-accent-foreground"
+          className="flex h-full w-full min-w-48 items-center justify-start gap-20 whitespace-nowrap rounded-[0.5rem] border border-input bg-background px-4 py-2 text-sm font-normal text-muted-foreground shadow-none transition-colors hover:bg-accent hover:text-accent-foreground"
         >
           Quick Actions...
-          <kbd className="inline-flex h-5 items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[11px] font-medium text-muted-foreground">
+          <kbd className="hidden h-5 items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[11px] font-medium text-muted-foreground md:inline-flex">
             <span className="text-[13px]">⌘</span>
             <span>K</span>
           </kbd>
